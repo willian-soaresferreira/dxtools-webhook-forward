b21d13df1b9240716aba96fa9051b7f411a04d95
willian-soaresferreira <willian.soaresferreira@gmail.com>
2022-12-21 13:18:25 -0300

    improvement: small console.log message adjustment

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,7 @@ app.listen(port, () => {
   const coloredAppName = "\x1b[32m".concat(appName, "\x1b[0m");
   const coloredWebpageName = "\x1b[32m".concat(webpageName, "\x1b[0m");
 
-  console.log(`${coloredAppName} app is running!\n`);
+  console.log(`${coloredAppName} app is running!`);
   console.log(
     `keep the ${coloredWebpageName} web page open to start forwarding the webhook events. \n`
   );
